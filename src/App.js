5db52d7b0006f1eae9fa2c1636a4c9441e12c74c
dlo <lg.ghijselings@gmail.com>
2023-03-09 11:13:30 +0100

    Map notes on App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
-
-      <Note />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
